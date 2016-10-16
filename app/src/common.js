926c1e91d0c25202e7d17f4b8ea46a2520558eee
ahonn <ahonn95@outlook.com>
2016-10-16 13:01:39 +0800

    add school select

---
+++ app/src/common.js
@@ -0,0 +1,21 @@
+module.exports = {
+  message: {
+    keepActive: "在线中",
+    loginSuccess: "登录成功",
+    passwordError: "密码错误"
+  },
+  config: {
+    school: [
+      {
+        name: "广东东软学院",
+        nasip: "219.128.230.1",
+        icode: "10"
+      },
+      {
+        name: "电子科技大学中山学院",
+        nasip: "61.142.108.88",
+        icode: "172"
+      }
+    ]
+  }
+}
